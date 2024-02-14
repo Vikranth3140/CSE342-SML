7a030e9be2a088f1cda388e8f6833a583675880d
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-14 22:23:55 +0530

    Updated builde code

--- data_builder.py
+++ data_builder.py
@@ -1,20 +1,30 @@
 import numpy as np
 import csv
-from numpy import load
 
-data = load('mnist.npz')
-lst = data.files
+# Load MNIST data
+mnist_data = np.load('mnist.npz')
 
 # Specify the CSV file path
 csv_file = 'data.csv'
 
+# Extract data keys
+data_keys = mnist_data.files
+
 # Open the CSV file in write mode
-with open(csv_file, 'w', newline='') as file:
-    writer = csv.writer(file)
+with open(csv_file, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
 
     # Write the column headers
-    writer.writerow(['x_test', 'x_train', 'y_train', 'y_test'])
+    csv_writer.writerow(['x_test', 'x_train', 'y_train', 'y_test'])
 
     # Write the data rows
-    for i in range(len(data['x_test'])):
-        writer.writerow([data['x_test'][i], data['x_train'][i], data['y_train'][i], data['y_test'][i]])
+    for i in range(len(mnist_data['x_test'])):
+        x_test = mnist_data['x_test'][i]
+        x_train = mnist_data['x_train'][i]
+        y_train = mnist_data['y_train'][i]
+        y_test = mnist_data['y_test'][i]
+        
+        # Write data row
+        csv_writer.writerow([x_test.tolist(), x_train.tolist(), y_train.tolist(), y_test.tolist()])
+
+print("CSV file created successfully.")
