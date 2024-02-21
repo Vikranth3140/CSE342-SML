df9afca52883e33cdca9ecaf342090c3d3f220e9
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-21 19:45:35 +0530

    Final Q1

--- A1/Q1/q2.py
+++ A1/Q1/q2.py
@@ -8,13 +8,13 @@ with np.load('../mnist.npz') as mnist_data:
     x_train = mnist_data['x_train']
     y_train = mnist_data['y_train']
 
-fig, axs = plt.subplots(NUM_CLASSES, NUM_SAMPLES, figsize=(10, 20))
+    ig, axs = plt.subplots(NUM_CLASSES, NUM_SAMPLES, figsize=(10, 20))
 
-for i in range(NUM_CLASSES):
-    samples = x_train[y_train == i][:NUM_SAMPLES]
+    for i in range(NUM_CLASSES):
+        samples = x_train[y_train == i][:NUM_SAMPLES]
 
-    for j in range(NUM_SAMPLES):
-        axs[i, j].imshow(samples[j], cmap='gray')
-        axs[i, j].axis('off')
+        for j in range(NUM_SAMPLES):
+            axs[i, j].imshow(samples[j], cmap='gray')
+            axs[i, j].axis('off')
 
-plt.show()
+    plt.show()
--- A1/Q1/q3.py
+++ A1/Q1/q3.py
@@ -2,7 +2,6 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 def load_mnist_data():
